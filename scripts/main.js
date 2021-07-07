5a7cc3071d786d8c1d5de9478fc6b6eec9c238a9
Christian Alamillo <calam002@ucr.edu>
2021-07-06 19:02:49 -0700

    Changed event listener from manipulating inner text to creating and appending divs

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ const mainCalculator = document.querySelector('.main-calculator');
 
 mainCalculator.querySelectorAll('button').forEach(button => {
     button.addEventListener('click', function() {
-        mainDisplay.innerText = button.dataset.key;
+        const newElement = document.createElement('div')
+        newElement.innerText = button.dataset.key;
+        mainDisplay.appendChild(newElement);
     });
 });
