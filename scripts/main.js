660747936b2db9bb72051e0aa22c2b9aa357afc6
Christian Alamillo <calam002@ucr.edu>
2021-07-07 21:20:24 -0700

    Implemented clear() function that empties mainDisplay string

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,8 @@ const mainCalculator = document.querySelector('.main-calculator');
 const numbers = document.querySelectorAll('[data-num]');
 const operators = document.querySelectorAll('[data-op]');
 const del = document.querySelector('[data-delete]');
+const clear = document.querySelector('[data-clear]');
+const execute = document.querySelector('[data-equal]');
 
 function display() {
     //Permits user to enter only one decimal
@@ -10,25 +12,39 @@ function display() {
         return;
     }
     //changes inital value to first number clicked
-    if (mainDisplay.childNodes.length === 0) {
+    if (mainDisplay.innerText === '0') {
+    
         mainDisplay.innerText = this.dataset.num;
     }
     //appends number to current value
     else {
         mainDisplay.innerText += this.dataset.num.toString();
-    }    
-   // console.log(mainDisplay);
+    };
 }
 
 //removes last digit of number
 function deleteNumber() {
+    if (mainDisplay.innerText === '0') {
+        return;
+    }
+
     mainDisplay.innerText = mainDisplay.innerText.slice(0, -1);
+    
+    if (mainDisplay.innerText.length === 0) {
+        clearNumbers();
+    }   
+}
+
+function clearNumbers() {
+    mainDisplay.innerText = '0';
 }
 
 numbers.forEach(number => {
     number.onclick = display;
+
 })
 
-del.onclick = deleteNumber;
 
+del.onclick = deleteNumber;
+clear.onclick = clearNumbers;
 
