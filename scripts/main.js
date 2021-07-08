55e207b3c3c8d80b73d87f01b7a129f2f5b71b28
Christian Alamillo <calam002@ucr.edu>
2021-07-07 20:09:17 -0700

    Initialed numbers variable to contain nodelist of data-num elements and implemented function to change mainDisplay

--- scripts/main.js
+++ scripts/main.js
@@ -2,4 +2,13 @@ const mainDisplay = document.querySelector('#main-display');
 const mainCalculator = document.querySelector('.main-calculator');
 const numbers = document.querySelectorAll('[data-num]');
 
+function display() {
+    mainDisplay.innerText = this.dataset.num;
+}
+
+numbers.forEach(number => {
+    number.onclick = display;
+})
+
+
 console.log(numbers);
