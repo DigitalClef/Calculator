17bfbfcda35b689551ff3bb1879a21ddd7ba0d41
Christian Alamillo <calam002@ucr.edu>
2021-07-07 20:03:54 -0700

    Categorized divs into separate data attributes

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,5 @@
 const mainDisplay = document.querySelector('#main-display');
 const mainCalculator = document.querySelector('.main-calculator');
-const userNumber = document.createElement('div');;
-mainCalculator.querySelectorAll('[data-key').forEach(button => {
-    button.addEventListener('click', function() {
-        const userNumber = document.createElement('div');
-        userNumber.innerText = button.dataset.key;
-        mainDisplay.appendChild(userNumber);
-        }
-    );
-    console.log(userNumber);
-});
+const numbers = document.querySelectorAll('[data-num]');
+
+console.log(numbers);
