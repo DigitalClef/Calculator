16d939ebdbc7c54e336fa35dd799ca1b0401d636
Christian Alamillo <calam002@ucr.edu>
2021-07-10 12:01:42 -0700

    Implemented more specfic functions and fixed division and subtraction bugs

--- scripts/main.js
+++ scripts/main.js
@@ -1,96 +1,61 @@
 const mainDisplay = document.querySelector('#main-display');
-let previousOperand = 0;
+let previousOperand = '';
 let currentOperand = '';
-let currentOperator = '';
+let currentOperator = undefined;
 const numbers = document.querySelectorAll('[data-num]');
 const operators = document.querySelectorAll('[data-op]');
 const del = document.querySelector('[data-delete]');
 const clear = document.querySelector('[data-clear]');
 const execute = document.querySelector('[data-equal]');
 
-// function display() {
-//     //Permits user to enter only one decimal
-//     if (this.dataset.num === '.' && mainDisplay.innerText.includes('.')) {
-//         return;
-//     }
-    
-
-//     //changes inital value to first number clicked
-//     if (mainDisplay.innerText === '0') {
-    
-//         mainDisplay.innerText = this.dataset.num;
-//     }
-//     //appends number to current value
-//     else {
-//         mainDisplay.innerText += this.dataset.num.toString();
-//     };
-// }
-
-
-
-function appendNum() {
-    currentOperand += this.dataset.num.toString();
-    mainDisplay.innerText = currentOperand;
-    
+function display(value) {
+    mainDisplay.innerText = value;
 }
 
-function operation() {
-    previousOperand = parseFloat(mainDisplay.innerText);
-    currentOperand = '';
-    currentOperator = this.dataset.op;
-
-}
-
-//Performs corresponding operation on the two numbers
-function executeOperation() {
-    currentOperand = mainDisplay.innerText;
-    console.log(previousOperand)
-    console.log(currentOperand);
-    switch (currentOperator) {
-        
-        case '+':
-            mainDisplay.innerText = previousOperand + currentOperand;
-            break;
-        case '-':
-            mainDisplay.innerText = previousOperand - currentOperand;
-            break;
-        case '÷':
-            mainDisplay.innerText = previousOperand / currentOperand;
-
-            break;
-        case 'x':
-            mainDisplay.innerText = previousOperand * currentOperand;
-            break;
-    }
-
-    previousOperand = currentOperand;
-}
-
-//removes last digit of number
-function deleteNumber() {
-    if (mainDisplay.innerText === '0') {
+function appendNumber() {
+    //implemented so that numbers do not exceed display window of calculator
+    if (currentOperand.length >= 14) {
         return;
     }
-    mainDisplay.innerText = mainDisplay.innerText.slice(0, -1);  
-
-    if (mainDisplay.innerText.length === 0) {
-        clearNumbers();
-    }   
+    currentOperand += this.innerText.toString();
+    display(currentOperand);
 }
 
-function clearNumbers() {
+function setOperator() {
+    currentOperator = this.innerText;
+    previousOperand = currentOperand;
     currentOperand = '';
-    mainDisplay.innerText = '0';
 }
 
-numbers.forEach(number => {
-    number.onclick = appendNum;
-})
-operators.forEach(number => {
-    number.onclick = operation;
-})
+function solve() {
+    let final = 0
+    let prev = parseFloat(previousOperand);
+    let curr = parseFloat(currentOperand);
+    
+    switch (currentOperator) {
+        case '÷': 
+            final = prev / curr;
+            break;
+        case '+':
+            final = prev + curr;
+            break;
+        case '-':
+            final = prev - curr;
+            break;
+        case 'x':
+            final = prev * curr;
+            break;
+    }
+    display(final);
+    previousOperand = final;
+}
 
-del.onclick = deleteNumber;
-clear.onclick = clearNumbers;
-execute.onclick = executeOperation;
+numbers.forEach(button => {
+    button.onclick = appendNumber;
+});
 
+operators.forEach(button => {
+    button.onclick = setOperator;
+});
+
+execute.onclick = solve;
